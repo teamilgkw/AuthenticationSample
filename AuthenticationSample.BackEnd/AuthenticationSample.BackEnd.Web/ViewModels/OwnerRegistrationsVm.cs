affbc7ec77f87862846e077057d9631cc3978e14
kurdi <sheriff.kurdi@email.com>
2021-10-26 23:17:04 +0200

    View Models

---
+++ AuthenticationSample.BackEnd/AuthenticationSample.BackEnd.Web/ViewModels/OwnerRegistrationsVm.cs
@@ -0,0 +1,11 @@
+namespace AuthenticationSample.BackEnd.Web.ViewModels
+{
+    public class OwnerRegistrationsVm
+    {
+        public string FullName { get; set; }
+        public string MobileNumber { get; set; }  
+        public string Email { get; set; }  
+        public string Password { get; set; }  
+        public string ConfirmPassword { get; set; }  
+    }
+}
