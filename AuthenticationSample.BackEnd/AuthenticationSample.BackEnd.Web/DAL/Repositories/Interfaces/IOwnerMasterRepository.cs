c4039b2cfca8ce2f971a10274c8198c668f810af
hesham.salem <hesham.salem@HESHAM-PC>
2021-10-27 10:51:13 +0200

    Added IOwnerMasterRepository

---
+++ AuthenticationSample.BackEnd/AuthenticationSample.BackEnd.Web/DAL/Repositories/Interfaces/IOwnerMasterRepository.cs
@@ -0,0 +1,21 @@
+﻿using AuthenticationSample.BackEnd.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AuthenticationSample.BackEnd.Web.DAL.Repositories.Interfaces
+{
+    public interface IOwnerMasterRepository
+    {
+        Task<OwnerMaster> GetById(string id);
+        Task<IEnumerable<OwnerMaster>> GetAll();
+        Task<IEnumerable<OwnerMaster>> Find(Expression<Func<OwnerMaster, bool>> expression);
+        Task Add(OwnerMaster entity);
+        Task AddRange(IEnumerable<OwnerMaster> entities);
+        Task Remove(OwnerMaster entity);
+        Task RemoveRange(IEnumerable<OwnerMaster> entities);
+        Task SaveOwnerMaster(OwnerMaster entity);
+    }
+}
---
+++ AuthenticationSample.BackEnd/AuthenticationSample.BackEnd.Web/DAL/Repositories/Services/OwnerMasterRepository.cs
@@ -0,0 +1,62 @@
+﻿using AuthenticationSample.BackEnd.Web.DAL.Repositories.Interfaces;
+using AuthenticationSample.BackEnd.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AuthenticationSample.BackEnd.Web.DAL.Repositories.Services
+{
+    public class OwnerMasterRepository// : IOwnerMasterRepository
+    {
+    //    private ApplicationContext context;
+    //    private DbSet<OwnerMaster> OwnerMasterEntity;
+    //    public OwnerMasterRepository(ApplicationContext context)
+    //    {
+    //        this.context = context;
+    //        OwnerMasterEntity = context.Set<OwnerMaster>();
+    //    }
+
+    //    public Task Add(OwnerMaster entity)
+    //    {
+    //        context.Employees.Add(entity);
+    //    }
+
+    //    public Task AddRange(IEnumerable<OwnerMaster> entities)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<IEnumerable<OwnerMaster>> Find(Expression<Func<OwnerMaster, bool>> expression)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<IEnumerable<OwnerMaster>> GetAll()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<OwnerMaster> GetById(string id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task Remove(OwnerMaster entity)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task RemoveRange(IEnumerable<OwnerMaster> entities)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task SaveOwnerMaster(OwnerMaster entity)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    }
+}
