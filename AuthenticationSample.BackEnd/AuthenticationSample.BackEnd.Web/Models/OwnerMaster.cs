90197de9e6c3a9bd16d857ca7e50e73ab9d65123
kurdi <sheriff.kurdi@email.com>
2021-10-27 11:59:04 +0200

    owner master id name

--- AuthenticationSample.BackEnd/AuthenticationSample.BackEnd.Web/Models/OwnerMaster.cs
+++ AuthenticationSample.BackEnd/AuthenticationSample.BackEnd.Web/Models/OwnerMaster.cs
@@ -7,7 +7,7 @@ namespace AuthenticationSample.BackEnd.Web.Models
     [Table("OwnerMaster")]
     public class OwnerMaster
     {
-        public string Id { get; set; }
+        public string ID { get; set; }
         public HashSet<LoginType> LoginTypes { get; set; }
         public string FullName { get; set; }
         public DateTime BirthDate { get; set; }
